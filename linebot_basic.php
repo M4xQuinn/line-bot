36a2d0e48401e3b991d0ff87e0fdba93f92ff03d
M4xQuinn <66687396+M4xQuinn@users.noreply.github.com>
2020-10-29 16:08:05 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'lJ68LHtR3N2kOkgqUa9VhG3MdjDeGuebOfJNPzUcLxWwt993YUc5Xbq8o4Pz5xae2yN944yBOqitExgXonhRrUMTPVJq0MBtPmT50WP+SYcfY6G3yFPzTlqyxmb8rJoQuI50bo+Oag+dfNmUCbm7PQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
